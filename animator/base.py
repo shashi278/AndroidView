d2e930552a822d79fbf3796e79bd9957fd03a27c
shashi278 <shashiranjankv@gmail.com>
2020-04-07 14:06:27 +0530

    Animator directly animates the object itself

--- animator/base.py
+++ animator/base.py
@@ -22,11 +22,13 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 """
 from kivy.clock import Clock
+from kivy.graphics import PushMatrix, PopMatrix, Color, Rectangle, Rotate
 from functools import partial
 import time
 
-class Animator():
-	"""
+
+class Animator:
+    """
 	Animator
 	========
 	Base Class for Animators
@@ -49,29 +51,57 @@ class Animator():
 
 	"""
 
-	def __init__(self, widget, duration=1, repeat=True):
-		self.widget= widget
-		self.duration= duration
-		self._repeat= repeat
-		self._original= {}
-		self.attr=['opacity','height','width','pos_hint','angle']
+    def __init__(self, widget, duration=1, repeat=True):
+        self.widget = widget
+        self.duration = duration
+        self._repeat = repeat
+        self._original = {}
+        self.attr = ["opacity", "height", "width", "pos_hint", "angle"]
 
-		for key in self.attr:
-			self._original[key]= getattr(self.widget, key)
+        setattr(self.widget, "origin_", "")
+        setattr(self.widget, "angle", 0)
+        setattr(self.widget, "axis", tuple((0, 0, 1)))
 
-	@classmethod
-	def anim_complete(cls, obj, inst, widget):
-		if obj._repeat:
-			for key, val in obj._original.items():
-				setattr(widget, key, val)
+        for key in self.attr:
+            self._original[key] = getattr(self.widget, key)
 
-			inst.unbind(on_complete= obj.anim_complete)
-			time.sleep(0.3)		#just to make repeatition visually clear
-			Clock.schedule_once(partial(obj.start_, ), 0)
+    @classmethod
+    def anim_complete(cls, obj, inst, widget):
+        if obj._repeat:
+            for key, val in obj._original.items():
+                setattr(widget, key, val)
+            obj._update_canvas()
 
-		else:
-			inst.stop(widget)
+            inst.unbind(on_complete=obj.anim_complete)
+            time.sleep(0.3)  # just to make repeatition visually clear
+            Clock.schedule_once(partial(obj.start_,), 0)
 
-	def _initialize(self, **kwargs):
-		for key, val in kwargs.items():
-			setattr(self.widget, key, val)
+        else:
+            inst.stop(widget)
+
+    def _initialize(self, **kwargs):
+        for key, val in kwargs.items():
+            setattr(self.widget, key, val)
+        self._update_canvas()
+
+    def _animate(self, anim_obj):
+        self._update_canvas()  # this is to reset any previous animation value
+        anim_obj.cancel_all(self.widget)
+        anim_obj.bind(on_progress=self._update_canvas)
+        anim_obj.bind(on_complete=partial(self.anim_complete, self))
+        anim_obj.start(self.widget)
+
+    def _update_canvas(self, *args):
+        self.widget.canvas.before.remove_group("animator_group")
+        with self.widget.canvas.before:
+            PushMatrix(group="animator_group")
+            dsh = {
+                "axis": getattr(self.widget, "axis"),
+                "angle": getattr(self.widget, "angle"),
+                "origin": getattr(self.widget, "origin_")
+                or getattr(self.widget, "center"),
+            }
+            Rotate(**dsh, group="animator_group")
+        self.widget.canvas.after.remove_group("animator_group")
+        with self.widget.canvas.after:
+            PopMatrix(group="animator_group")
--- animator/fadeout.py
+++ animator/fadeout.py
@@ -26,101 +26,96 @@ from functools import partial
 
 from .base import Animator
 
-__all__= ('FadeOutAnimator', 'FadeOutDownAnimator', 'FadeOutLeftAnimator', 'FadeOutRightAnimator', 'FadeOutUpAnimator')
+__all__ = (
+    "FadeOutAnimator",
+    "FadeOutDownAnimator",
+    "FadeOutLeftAnimator",
+    "FadeOutRightAnimator",
+    "FadeOutUpAnimator",
+)
 
-#fade out
+# fade out
 class FadeOutAnimator(Animator):
-	def start_(self, tmp=None):
-		props=["opacity",]
-		vals=[1,]
-		self._initialize(**dict(zip(props, vals)))
-		
-		vals=[0,]
-		anim= Animation(
-			d= self.duration,
-			**dict(zip(props, vals)),
-			)
+    def start_(self, tmp=None):
+        props = [
+            "opacity",
+        ]
+        vals = [
+            1,
+        ]
+        self._initialize(**dict(zip(props, vals)))
 
-		anim.cancel_all(self.widget)
-		anim.start(self.widget)
-		anim.bind(on_complete= partial(self.anim_complete, self))
+        vals = [
+            0,
+        ]
+        anim = Animation(d=self.duration, **dict(zip(props, vals)),)
+
+        self._animate(anim)
+
 
 class FadeOutDownAnimator(Animator):
-	def start_(self, tmp=None):
-		props=["opacity", "pos_hint"]
+    def start_(self, tmp=None):
+        props = ["opacity", "pos_hint"]
 
-		__tmp={}
-		for key, val in self._original["pos_hint"].items():
-			if key in ["center_y", "y", "top"]:
-				__tmp[key]= val-0.2
-			else: __tmp[key]= val
+        __tmp = {}
+        for key, val in self._original["pos_hint"].items():
+            if key in ["center_y", "y", "top"]:
+                __tmp[key] = val - 0.2
+            else:
+                __tmp[key] = val
 
-		vals=[0,__tmp]
-		anim= Animation(
-			d= self.duration,
-			**dict(zip(props, vals)),
-			)
+        vals = [0, __tmp]
+        anim = Animation(d=self.duration, **dict(zip(props, vals)),)
 
-		anim.cancel_all(self.widget)
-		anim.start(self.widget)
-		anim.bind(on_complete= partial(self.anim_complete, self))
+        self._animate(anim)
+
 
 class FadeOutLeftAnimator(Animator):
-	def start_(self, tmp=None):
-		props=["opacity", "pos_hint"]
+    def start_(self, tmp=None):
+        props = ["opacity", "pos_hint"]
 
-		__tmp={}
-		for key, val in self._original["pos_hint"].items():
-			if key in ["center_x", "x", "left"]:
-				__tmp[key]= val-0.2
-			else: __tmp[key]= val
+        __tmp = {}
+        for key, val in self._original["pos_hint"].items():
+            if key in ["center_x", "x", "left"]:
+                __tmp[key] = val - 0.2
+            else:
+                __tmp[key] = val
 
-		vals=[0,__tmp]
-		anim= Animation(
-			d= self.duration,
-			**dict(zip(props, vals)),
-			)
+        vals = [0, __tmp]
+        anim = Animation(d=self.duration, **dict(zip(props, vals)),)
 
-		anim.cancel_all(self.widget)
-		anim.start(self.widget)
-		anim.bind(on_complete= partial(self.anim_complete, self))
+        self._animate(anim)
+
 
 class FadeOutRightAnimator(Animator):
-	def start_(self, tmp=None):
-		props=["opacity", "pos_hint"]
+    def start_(self, tmp=None):
+        props = ["opacity", "pos_hint"]
 
-		__tmp={}
-		for key, val in self._original["pos_hint"].items():
-			if key in ["center_x", "x", "left"]:
-				__tmp[key]= val+0.2
-			else: __tmp[key]= val
+        __tmp = {}
+        for key, val in self._original["pos_hint"].items():
+            if key in ["center_x", "x", "left"]:
+                __tmp[key] = val + 0.2
+            else:
+                __tmp[key] = val
 
-		vals=[0,__tmp]
-		anim= Animation(
-			d= self.duration,
-			**dict(zip(props, vals)),
-			)
+        vals = [0, __tmp]
+        anim = Animation(d=self.duration, **dict(zip(props, vals)),)
 
-		anim.cancel_all(self.widget)
-		anim.start(self.widget)
-		anim.bind(on_complete= partial(self.anim_complete, self))
+        self._animate(anim)
+
 
 class FadeOutUpAnimator(Animator):
-	def start_(self, tmp=None):
-		props=["opacity", "pos_hint"]
+    def start_(self, tmp=None):
+        props = ["opacity", "pos_hint"]
 
-		__tmp={}
-		for key, val in self._original["pos_hint"].items():
-			if key in ["center_y", "y", "top"]:
-				__tmp[key]= val+0.2
-			else: __tmp[key]= val
+        __tmp = {}
+        for key, val in self._original["pos_hint"].items():
+            if key in ["center_y", "y", "top"]:
+                __tmp[key] = val + 0.2
+            else:
+                __tmp[key] = val
 
-		vals=[0,__tmp]
-		anim= Animation(
-			d= self.duration,
-			**dict(zip(props, vals)),
-			)
+        vals = [0, __tmp]
+        anim = Animation(d=self.duration, **dict(zip(props, vals)),)
 
-		anim.cancel_all(self.widget)
-		anim.start(self.widget)
-		anim.bind(on_complete= partial(self.anim_complete, self))
+        self._animate(anim)
